e127b8424bf2fad9f9789a5bcc3f3bf605e04f31
GautierBlandin <gautier.blandin@gmail.com>
2022-08-12 21:13:02 +0200

    Add shape computation function

---
+++ src/_shape.py
@@ -0,0 +1,38 @@
+import src._size as _size
+
+
+def compute_shape(data: list):
+    """
+    Computes the shape of the given data.
+    """
+    shape = []
+    arr = data
+    while type(arr) == list:
+        if len(arr) == 0:
+            raise ValueError("Every list in the data must have length > 0.")
+        shape.append(len(arr))
+        arr = arr[0]
+    return shape
+
+
+def compute_step_size(shape: list[int]) -> list[int]:
+    """
+    Computes the step size of an array with the given shape.
+    """
+    size = _size.compute_size(shape)
+    step = size
+    step_sizes = []
+    for i in shape:
+        step = step // i
+        step_sizes.append(step)
+    return step_sizes
+
+
+def check_integrity(shape, data):
+    """
+    Checks the integrity of the given data.
+    """
+    size = _size.compute_size(shape)
+    if len(data) != size:
+        raise ValueError("The data has the wrong size.")
+    return True
