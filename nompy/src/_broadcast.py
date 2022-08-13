174c33036cdb0641a364b990031af53c770eb773
GautierBlandin <gautier.blandin@gmail.com>
2022-08-14 01:39:34 +0200

    Add general operation broadcasting functions

--- nompy/src/_broadcast.py
+++ nompy/src/_broadcast.py
@@ -1,3 +1,11 @@
+from nompy.src.array_creation import zeros, array
+from nompy.src._shape import iter_ndim_shape
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from nompy.src.ndarray import ndarray
+
+
 def is_broadcastable(shape1, shape2):
     n = min(len(shape1), len(shape2))
     for i in range(1, n+1):
@@ -29,3 +37,33 @@ def broadcast(shape1, shape2):
     for i in range(n+1, m+1):
         shape[-i] = larger[-i]
     return shape
+
+
+def broadcast_function_array(array1, array2, operation):
+    if is_broadcastable(array1.shape, array2.shape):
+        result_shape = broadcast(array1.shape, array2.shape)
+        result = zeros(result_shape)
+        for pos in iter_ndim_shape(result.shape):
+            result[pos] = operation(array1._get_broadcast_item(pos), array2._get_broadcast_item(pos))
+        return result
+    else:
+        raise ValueError("Unsupported Operation")
+
+
+def broadcast_function_value(arr, value, operation):
+    if isinstance(value, arr.dtype):
+        return broadcast_function_array(array([value]), arr, operation)
+    else: # Try to convert value to the same type as arr
+        try:
+            value = arr.dtype(value)
+            return broadcast_function_array(array([value]), arr, operation)
+        except Exception:
+            raise ValueError("Invalid data type")
+
+
+def broadcast_function(arr, other, operation):
+    from nompy.src.ndarray import ndarray
+    if isinstance(other, ndarray):
+        return broadcast_function_array(arr, other, operation)
+    else:
+        return broadcast_function_value(arr, other, operation)
