da110cfecc9122035adb9d6a02d6ffa74850f232
GautierBlandin <gautier.blandin@gmail.com>
2022-08-14 01:38:08 +0200

    Add the possibility to initialize step sizes

--- nompy/src/ndarray/__init__.py
+++ nompy/src/ndarray/__init__.py
@@ -3,11 +3,14 @@ import nompy.src._shape as _shape
 
 
 class ndarray:
-    def __init__(self, shape, data, dtype:type = None):
+    def __init__(self, shape, data, dtype:type = None, step_sizes=None):
         self.shape = shape
         self.data = data
         self.size = _size.compute_size(shape)
-        self.step_sizes = _shape.compute_step_size(shape)
+        if step_sizes is None:
+            self.step_sizes = _shape.compute_step_size(shape)
+        else:
+            self.step_sizes = step_sizes
         if dtype is not None:
             self.dtype = dtype
         else:
