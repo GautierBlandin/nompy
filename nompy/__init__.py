53f8c2eaeb24d4c544092bcefea5e7ede4cedc38
GautierBlandin <gautier.blandin@gmail.com>
2022-08-14 17:42:32 +0200

    Add imports to nompy init file

--- nompy/__init__.py
+++ nompy/__init__.py
@@ -1,2 +1,6 @@
-from nompy.src.array_creation import array, zeros, ones
+from nompy.src.array_creation import array, zeros, ones, arange
 from nompy.src.ndarray import ndarray
+from nompy.src.dot import dot
+from nompy.src.equality import array_equal
+
+import nompy.src.linalg as linalg
