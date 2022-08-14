3359fb5f4191553367461705e8293a96174d3dac
GautierBlandin <gautier.blandin@gmail.com>
2022-08-14 17:42:19 +0200

    Add arange function

--- nompy/src/array_creation.py
+++ nompy/src/array_creation.py
@@ -33,3 +33,14 @@ def array(data: list, dtype: type = float):
     flattened = [dtype(x) for x in flattened]
     _shape.check_integrity(shape, flattened)
     return ndarray(shape, flattened)
+
+
+def arange(arg1, end=None, step=None):
+    """ Create a 1d nompy array with data identical to range(arg1, end, step). """
+    if step is not None and end is not None:
+        return array(list(range(arg1, end, step)))
+
+    if end is not None:
+        return array(list(range(arg1, end)))
+
+    return array(list(range(arg1)))
