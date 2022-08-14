916c1ecee9f722b0c39ffeadd8c4649b41bae75f
GautierBlandin <gautier.blandin@gmail.com>
2022-08-14 17:40:20 +0200

    Update transpose to set base if necessary

--- nompy/src/ndarray/transpose.py
+++ nompy/src/ndarray/transpose.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
     from nompy.src.ndarray import ndarray
 
@@ -8,12 +9,18 @@ def transpose(self: ndarray, permutation=None):
     from nompy.src.ndarray import ndarray
     if permutation is None:
         new_shape = list(reversed(self.shape))
-        new_step_sizes = list(reversed(self.step_sizes))
-        return ndarray(new_shape, self.data, dtype=self.dtype, step_sizes=new_step_sizes)
+        new_step_sizes = list(reversed(self.strides))
+        if self.base is not None:
+            return ndarray(new_shape, self.data, dtype=self.dtype, strides=new_step_sizes, base=self.base)
+        else:
+            return ndarray(new_shape, self.data, dtype=self.dtype, strides=new_step_sizes, base=self)
     else:
-        new_step_sizes = [0] * len(self.step_sizes)
+        new_step_sizes = [0] * len(self.strides)
         new_shape = [0] * len(self.shape)
         for i, p in enumerate(permutation):
-            new_step_sizes[i] = self.step_sizes[p]
+            new_step_sizes[i] = self.strides[p]
             new_shape[i] = self.shape[p]
-        return ndarray(new_shape, self.data, dtype=self.dtype, step_sizes=new_step_sizes)
+        if self.base is not None:
+            return ndarray(new_shape, self.data, dtype=self.dtype, strides=new_step_sizes, base=self.base)
+        else:
+            return ndarray(new_shape, self.data, dtype=self.dtype, strides=new_step_sizes, base=self)
