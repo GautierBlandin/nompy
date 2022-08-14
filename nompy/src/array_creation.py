500bdb4018bdc816e90b0fcad9ba291b22d4ffd0
GautierBlandin <gautier.blandin@gmail.com>
2022-08-14 17:42:13 +0200

    Set float as the default dtype in array creation

--- nompy/src/array_creation.py
+++ nompy/src/array_creation.py
@@ -3,32 +3,33 @@ import nompy.src._shape as _shape
 import nompy.src._flatten as _flatten
 
 
-def zeros(shape: list[int]):
+def zeros(shape: list[int], dtype: type = float):
     from nompy.src.ndarray import ndarray
     """
     Creates a nompy array of zeros with the given shape.
     """
     size = _size.compute_size(shape)
-    data = [0] * size
+    data = [dtype(0)] * size
     return ndarray(shape, data)
 
 
-def ones(shape: list[int]):
+def ones(shape: list[int], dtype: type = float):
     from nompy.src.ndarray import ndarray
     """
     Creates a nompy array of ones with the given shape.
     """
     size = _size.compute_size(shape)
-    data = [1] * size
+    data = [dtype(1)] * size
     return ndarray(shape, data)
 
 
-def array(data: list):
+def array(data: list, dtype: type = float):
     from nompy.src.ndarray import ndarray
     """
-    Creates a nompy array with the given data and shape.
+    Create a nompy array with the given data and shape.
     """
     shape = _shape.compute_shape(data)
     flattened = _flatten.flatten(data)
+    flattened = [dtype(x) for x in flattened]
     _shape.check_integrity(shape, flattened)
     return ndarray(shape, flattened)
