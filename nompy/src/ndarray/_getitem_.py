14b0b505521f0e3151101a3d2e58a239b39ab51d
GautierBlandin <gautier.blandin@gmail.com>
2022-08-14 17:39:25 +0200

    Update get and set item to allow array transposition and reshape while preserving expected item order

--- nompy/src/ndarray/_getitem_.py
+++ nompy/src/ndarray/_getitem_.py
@@ -1,17 +1,29 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
     from nompy.src.ndarray import ndarray
+
+import nompy.src.multi_index as multi_index
 
 
 def __getitem__(self: ndarray, pos):
     if type(pos) == int and len(self.shape) == 1:
-        return self.data[pos]
+        if self.base is None:
+            return self.data[pos]
+        else:
+            return self.base._get_from_1d_index(pos)
 
     elif len(pos) == len(self.shape):
-        index = 0
-        for i, p in enumerate(pos):
-            index += p * self.step_sizes[i]
-        return self.data[index]
+        index = multi_index.pos_to_index(pos, self.strides)
+        if self.base is None:
+            return self.data[index]
+        else:
+            return self.base._get_from_1d_index(index)
     else:
         raise ValueError("Unsupported Operation")
+
+
+def _get_from_1d_index(self: ndarray, index: int):
+    pos = multi_index.index_to_pos(index, self.shape)
+    return self[pos]
--- nompy/src/ndarray/_setitem_.py
+++ nompy/src/ndarray/_setitem_.py
@@ -4,15 +4,27 @@ if TYPE_CHECKING:
     from nompy.src.ndarray import ndarray
 
 
+import nompy.src.multi_index as multi_index
+
+
 def __setitem__(self: ndarray, pos, value):
     if type(pos) == int and len(self.shape) == 1:
-        self.data[pos] = value
-        return
+        if self.base is None:
+            self.data[pos] = value
+        else:
+            self.base._set_from_1d_index(pos, value)
 
-    if len(pos) == len(self.shape):
-        index = 0
-        for i, p in enumerate(pos):
-            index += p * self.step_sizes[i]
-        self.data[index] = value
+    elif len(pos) == len(self.shape):
+        index = multi_index.pos_to_index(pos, self.strides)
+        if self.base is None:
+            self.data[index] = value
+        else:
+            self.base._get_from_1d_index(index, value)
+
     else:
         raise ValueError("Unsupported Operation")
+
+
+def _set_from_1d_index(self: ndarray, index: int, value):
+    pos = multi_index.index_to_pos(index, self.shape)
+    self[pos] = value
